8710455c15ba588892cfcad063ab772528938234
tori0359 <hij0359@gmail.com>
2019-08-14 12:52:10 +0900

    - Description :  1. LoginContoller - 추가  2. FindController - 추가 - Author	  : 황인준 - RegistDate  : 2019-08-14

--- RentalMe/src/main/java/com/me/rentalme/login/controller/LoginController.java
+++ RentalMe/src/main/java/com/me/rentalme/login/controller/LoginController.java
@@ -19,6 +19,7 @@ import com.me.rentalme.model.entity.UserVo;
 * @version ver1.0
 * @see 
 * 등록일자 : 2019.08.13
+* 수정일자 : 2019.08.14
 */
 @Controller
 public class LoginController {
@@ -29,9 +30,25 @@ public class LoginController {
 	Logger log = LoggerFactory.getLogger(getClass());
 	
 	/**
+	* 로그인 폼
+	* 
+	* @param  
+	* @return String 
+	* @author 황인준
+	* @exception 
+	*/
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		log.debug("로그인 폼 컨트롤러...");
+
+		return "login/login";
+	}
+	
+	/**
 	* 로그인
 	* 
 	* @param  UserVo
+	* @param  HttpServletRequest - session처리하기 위해.
 	* @return String 
 	* @author 황인준
 	* @exception 
@@ -40,6 +57,7 @@ public class LoginController {
 	public String login(UserVo bean, HttpServletRequest req) {
 		log.debug("로그인 컨트롤러...");
 		
+		//Service에서 세션 처리 
 		int result = loginService.login(bean, req);
 		
 		return "redirect:/";
