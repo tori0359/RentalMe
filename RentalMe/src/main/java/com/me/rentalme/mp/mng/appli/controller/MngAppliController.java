cf0ca1bdfae6eda1e7814955e905daebdc47bb1f
tori0359 <hij0359@gmail.com>
2019-08-14 17:19:33 +0900

    - Description : 마이페이지(관리자)  1. MngActController - 이벤트경매 컨트롤러 추가  2. MngAppliController - 상품리스트 컨트롤러 추가  3. MngCsController - 고객센터 컨트롤러 추가  4. MngDeclController - 신고접수 컨트롤러 추가  5. MngRentalController - 상품등록 컨트롤러 추가  6. MngUserController - 사용자관리 컨트롤러 추가  7. ProductVo - 상품Vo 추가 - Author	  : 황인준 - RegistDate  : 2019-08-14

---
+++ RentalMe/src/main/java/com/me/rentalme/mp/mng/appli/controller/MngAppliController.java
@@ -0,0 +1,144 @@
+package com.me.rentalme.mp.mng.appli.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+* 마이페이지(관리자) 리스트 컨트롤러
+* 
+* @author 황인준
+* @version ver1.0 
+* 등록일자 : 2019.08.14
+*/
+@Controller
+@RequestMapping("/mp/mng")
+public class MngAppliController {
+	
+	Logger log = LoggerFactory.getLogger(getClass());
+
+	
+	/**
+	* 마이페이지(관리자) - 메인 리스트
+	* 
+	* @param  
+	* @return ModelAndView 
+	* @author 황인준
+	* @exception 
+	*/
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView getMainList() {
+		log.debug("마이페이지(관리자) 메인 컨트롤러");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mp/manager/mngMainList");
+		return mav;
+	}
+	
+	/**
+	* 마이페이지(관리자) - 대형가전 리스트
+	* 
+	* @param  
+	* @return ModelAndView 
+	* @author 황인준
+	* @exception 
+	*/
+	@RequestMapping(value = "/Appli/lg", method = RequestMethod.GET)
+	public ModelAndView getLgList() {
+		log.debug("대형가전 리스트 컨트롤러");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mp/manager/mngAppliLgList");
+		return mav;
+	}
+	
+	/**
+	* 마이페이지(관리자) - 소형가전 리스트
+	* 
+	* @param  
+	* @return ModelAndView 
+	* @author 황인준
+	* @exception 
+	*/
+	@RequestMapping(value = "/Appli/sm", method = RequestMethod.GET)
+	public ModelAndView getSmList() {
+		log.debug("소형가전 리스트 컨트롤러");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mp/manager/mngAppliSmList");
+		return mav;
+	}
+	
+	/**
+	*  마이페이지(관리자) - 주방용품 리스트
+	* 
+	* @param  
+	* @return ModelAndView 
+	* @author 황인준
+	* @exception 
+	*/
+	@RequestMapping(value = "/Appli/kitc", method = RequestMethod.GET)
+	public ModelAndView getKitcList() {
+		log.debug("주방 리스트 컨트롤러");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mp/manager/mngAppliKitcList");
+		return mav;
+	}
+	
+	/**
+	*  마이페이지(관리자) - 가구 리스트
+	* 
+	* @param  
+	* @return ModelAndView 
+	* @author 황인준
+	* @exception 
+	*/
+	@RequestMapping(value = "/Appli/furn", method = RequestMethod.GET)
+	public ModelAndView getFurnList() {
+		log.debug("가구 리스트 컨트롤러");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mp/manager/mngAppliFurnList");
+		return mav;
+	}
+	
+	/**
+	*  마이페이지(관리자) - 기타 리스트
+	* 
+	* @param  
+	* @return ModelAndView 
+	* @author 황인준
+	* @exception 
+	*/
+	@RequestMapping(value = "/Appli/rest", method = RequestMethod.GET)
+	public ModelAndView getRestList() {
+		log.debug("기타 리스트 컨트롤러");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mp/manager/mngAppliRestList");
+		return mav;
+	}
+	
+	/**
+	*  마이페이지(관리자) - 패키지 리스트
+	* 
+	* @param  
+	* @return ModelAndView 
+	* @author 황인준
+	* @exception 
+	*/
+	@RequestMapping(value = "/Appli/pkg", method = RequestMethod.GET)
+	public ModelAndView getPkgList() {
+		log.debug("패키지 리스트 컨트롤러");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mp/manager/mngAppliPkgList");
+		return mav;
+	}
+
+	
+}
